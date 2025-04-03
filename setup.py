3b348080a32c759782669ea3344575bb0ea11047
kathale-snehal <beatuykarhale@gmail.com>
2025-04-03 12:32:58 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Educational Chatbot',
+    version= '0.0.0',
+    author= 'snehal kathale',
+    author_email= 'imcasnehal@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
